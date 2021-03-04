ff8241656bda5b4f1ef78a056406fc6c39902553
lathanliou <lltk2015@mymail.pomona.edu>
2021-03-04 09:34:50 -0500

    initial commit

---
+++ src/gatsby-theme-mate/icons.js
@@ -0,0 +1,21 @@
+import {
+  faMedium,
+  faTwitter,
+  faGithub,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
+import {
+  faGlobe,
+  faEnvelope,
+  faCoffee,
+} from '@fortawesome/free-solid-svg-icons';
+
+export const ICONS = [
+  faMedium,
+  faTwitter,
+  faGithub,
+  faInstagram,
+  faGlobe,
+  faEnvelope,
+  faCoffee,
+];
